d1e50ae0898e4bf2d0b61393a6f8121604189ec8
kowi1700 <kou.and.more@gmail.com>
2024-10-14 20:16:15 +0900

    ひとまずメインクラス叩き台 (出ていく処理と歩いてコインを増やす処理を実装)

--- src/Jasco/Main.java
+++ src/Jasco/Main.java
@@ -1,5 +1,30 @@
 package Jasco;
+//import Games.*;
 
 public class Main {
-
+	public static void main(String[] args) {
+		int coin = 0; //初期の所持コイン
+		boolean inJasco = true;
+		int selectNum = 0; //選択肢用
+		
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("[アナウンス]: ジャスコの100階へようこそ！夢のようなひとときをお過ごしください");
+		System.out.println("-----------------------------------------------------------------");
+		
+		do {
+			selectNum = Oparation.askCommand(coin);
+			switch(selectNum) {
+			case 0 :  inJasco = Oparation.goOut(coin);
+			break;
+			case 1 :  coin = Oparation.walk(coin);
+			break;
+			}
+			
+		}while(inJasco);
+		
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("[アナウンス]: さようなら。今度はもっとうまくやりましょうね");
+	}
+	
+	
 }
---
+++ src/Jasco/Oparation.java
@@ -0,0 +1,82 @@
+package Jasco;
+
+public class Oparation {
+	
+	//選択肢を尋ねるメソッド
+	public static int askCommand(int coin) {
+		int selectNum = -1;
+		
+		do {
+			System.out.printf("[ 所持金: %d coin ]  なにをする？%n", coin);
+			System.out.println("[0]: 出ていく [1]: 歩く [2]: (故障中) [3]: (故障中) [4]: (故障中)");
+			String input = new java.util.Scanner(System.in).nextLine();
+			try {
+				selectNum = Integer.parseInt(input);
+				return selectNum;
+			} catch(NumberFormatException e){
+				System.out.println("[アナウンス]: 申し訳ありません。どうか数字でお話ください");
+				return selectNum;
+			}
+		}while(selectNum < 0);
+	}
+	
+	//出ていくメソッド
+	public static boolean goOut(int coin) {
+		boolean inJasco = true;
+		int selectNum = -1;
+		
+		System.out.printf("[ 所持金: %d coin ]  おカネをぜんぶ捨てて、ほんとうに出ていく？%n", coin);
+		System.out.println("[0]: 出ていく [1]: ひきかえす");
+		String input = new java.util.Scanner(System.in).nextLine();
+		try {
+			selectNum = Integer.parseInt(input);
+			switch(selectNum) {
+			case 0 : 
+				if(coin > 0) {
+					System.out.printf("%d coin を失った。%n", coin);
+				}else if(coin <= 0) {
+					System.out.println("捨てるおカネもなくて、恥ずかしい");
+				}
+				inJasco = false;
+			break;	
+			case 1 : 
+				System.out.println("[アナウンス]: おかえりなさい！夢のようなひとときをお過ごしください");
+			break;
+			default : System.out.println("[アナウンス]: 申し訳ありません。聞き取れませんでした");
+			break;
+			}
+			
+		} catch(NumberFormatException e) {
+			System.out.println("[アナウンス]: 申し訳ありません。どうか数字でお話ください");
+			return inJasco;
+		}
+		return inJasco;
+		
+	}
+	
+	//歩くメソッド
+	public static int walk(int coin) {
+		System.out.printf("[ 所持金: %d coin ]  何歩くらい、あるいてみようか？%n", coin);
+		String input = new java.util.Scanner(System.in).nextLine();
+		try {
+			long steps = System.currentTimeMillis() + Integer.parseInt(input);
+			do {
+                System.out.printf("うろ%n");
+            } while (System.currentTimeMillis() < steps);
+			if(steps % 2 == 0) {
+				System.out.println();
+				System.out.println("あ、1coin ひろった！");
+				coin++;
+				return coin;
+			}else {
+				System.out.println();
+				System.out.println("同じところ、もどってきたみたい");
+				return coin;
+			}
+			
+		} catch(NumberFormatException e) {
+			System.out.println("そんなに、あるけないよ");
+			return coin;
+		}
+	}
+}
