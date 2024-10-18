3f3d7e44ce6503386570ed724ca76a6a7b22dda9
kowi1700 <kou.and.more@gmail.com>
2024-10-18 13:36:22 +0900

    セーブ機能、めちゃくちゃいいね！テンション上がってつい触らせてもらったよー。 簡単なバグ取りと例外処理追加したのと、PASSをPASSっぽくしてみた(好みの問題) コインの返却について絶対値使ったけど、セーブの解釈が違うかもしれない！また話そう！

--- src/jasco/Save.java
+++ src/jasco/Save.java
@@ -25,36 +25,54 @@ public class Save {
 
 	public static int save(int coin) {
 		System.out.print("4桁の好きな数字を入力してください　＞");
-		double id = new java.util.Scanner(System.in).nextInt();
-		double pass = coin * (1207 + id * 10) + 222 * id;
-		System.out.println("coinをお預かりいたしました");
-		System.out.println("ID:" + (int) id + " PASS:" + (int) pass);
-		System.out.println("次回、来店時にお伝えください");
-		System.out.print(" -" + coin + "coin	please enter　＞");
-		coin = 0;
-		new java.util.Scanner(System.in).nextLine();
+		String strId = new java.util.Scanner(System.in).nextLine();
+		try {	
+				if(strId.length() > 3 && strId.length() < 5) {
+					int id = Integer.parseInt(strId);
+					int pass = coin * (1207 + id * 10) + 222 * id;
+					String hexPass = Integer.toHexString(pass);
+					System.out.println("coinをお預かりいたしました");
+					System.out.println("ID:" + strId + " PASS:" + hexPass);
+					System.out.println("次回、来店時にお伝えください");
+					System.out.print(" -" + coin + "coin	please enter　＞");
+					coin = 0;
+					new java.util.Scanner(System.in).nextLine();
+			}else {
+				System.out.println("わたしは 4桁ではない数字は ぜんぶきらいです");
+				System.out.println("------------------------------------");
+			}
+		}catch(NumberFormatException e) {
+			System.out.println("ああ　読めませんでしたか 申し訳ございません");
+			System.out.println("　　　す   う　　じ　　　をいれてくださいね");
+			System.out.println("------------------------------------");
+		}
 		return coin;
 	}
 
 	public static int load(int coin) {
 		System.out.print("4桁のIDを入力してください　＞");
-		double id = new java.util.Scanner(System.in).nextInt();
+		String strId = new java.util.Scanner(System.in).nextLine();
+		int id = Integer.parseInt(strId);
 		System.out.print("PASSを入力してください　＞");
-		double pass = new java.util.Scanner(System.in).nextInt();
-		double temp = (pass - 222 * id) / (1207 + id * 10);
+		String hexPass = new java.util.Scanner(System.in).nextLine();
+		int pass = Integer.parseInt(hexPass, 16);
+		int temp = (pass - 222 * id) / (1207 + id * 10);
 		if (temp % 1 != 0) {
 			System.out.println("不正な入力です");
 			return coin;
 		}
 		System.out.print("coinをご返却致します");
-		if (temp > coin)
-			System.out.println(" +" + (temp - coin) + "coin　 please enter　＞");
-		if (coin > temp)
-			System.out.println(temp - coin + "coin	please enter　＞");
-		if (coin == temp)
+		if (temp > coin) {
+			System.out.println(" +" + (int)(temp - coin) + "coin　 please enter　＞");
+		}
+		if (coin > temp) {
+			System.out.println((int)(temp - coin) + "coin	please enter　＞");
+		}
+		if (coin == temp) {
 			System.out.println("±0coin	please enter　＞");
+		}
 		System.out.println("夢のような時間をお過ごしください");
-		coin = (int) temp;
+		coin += Math.abs(temp - coin);
 		return coin;
 	}
 
